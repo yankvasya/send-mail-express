64641102641c7a475ea0cad62d7d505ffadb192a
yankvasya <yankvasya@gmail.com>
2022-09-16 22:27:08 +0300

    feat: added `POST /mail`

--- src/index.js
+++ src/index.js
@@ -38,6 +38,43 @@ const configureApiEndpoints = (app) => {
   app.get("/", (req, res) => {
     res.send("Welcome to express-create application! ");
   });
+
+  app.post("/mail", (req, res) => {
+    const { email, phone, name } = req.body;
+    const text =
+      (process.env.MAIL_TITLE || "Сообщение с формы") +
+      "\n" +
+      `Имя: ${name}\n` +
+      `Почта: ${email}\n` +
+      `Телефон: ${phone}`;
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL || "",
+        pass: process.env.PASS || "",
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.USER || "",
+      to: process.env.TO || "",
+      subject: process.env.SUBJECT || "",
+      text,
+    };
+
+    transporter
+      .sendMail(mailOptions)
+      .then((info) => {
+        res.send(
+          `Сообщение на почту ${info.accepted[0]} было успешно отправлено`
+        );
+      })
+      .catch(console.error);
+  });
 };
 
 init();
